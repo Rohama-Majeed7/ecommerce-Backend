638a8cfeaeb23fe17c7bfe1a23552060eef6ca1e
Rohama Majeed <rohamamajeed3@gmail.com>
2026-06-09 17:04:00 +0300

    set files

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,20 +2,20 @@ import jwt from "jsonwebtoken";
 
 async function authMiddleware(req, res, next) {
   try {
-    // const authHeader = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-    // if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    //   return res.status(401).json({ msg: "Please login" });
-    // }
-    const token = req.cookies.token;
-    if (!token) {
-      return res.status(401).json({
-        success: false,
-        message: "Please login first",
-      });
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ msg: "Please login" });
     }
+    // const token = req.cookies.token;
+    // if (!token) {
+    //   return res.status(401).json({
+    //     success: false,
+    //     message: "Please login first",
+    //   });
+    // }
 
-    // const token = authHeader.split(" ")[1];
+    const token = authHeader.split(" ")[1];
 
     const user = jwt.verify(token, process.env.JWT_SECRET || "helloromi");
 
